1ee4ad78297a667bd027c7a09ee6c273359cbc53
DassevilleJuanB <36193626+DassevilleJuanB@users.noreply.github.com>
2018-02-06 10:16:03 -0300

    Ejercicios 1 y 2
    Primera Clase

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,22 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
+	//1ro Defino las variables
+	var nombre;
+
+	//2do Les asigno valor
+	nombre = prompt("ingrese nombre","natalia natalia");
+
+	//3ro Las uso
+	alert("su nombre es: " + nombre);
 	
+	/*
+	esto es un bloque
+	alert(nombre);
+	alert("nombre");
+	nombre = "Juan Bautista Dasseville";
+	alert(nombre);
+	var person = prompt("Please enter your name", "Harry Potter");
+	*/
 }
 
