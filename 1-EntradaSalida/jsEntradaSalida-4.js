38d612384c1a12ad0f31a497d91bd1d75e63e543
DassevilleJuanB <36193626+DassevilleJuanB@users.noreply.github.com>
2018-02-06 12:32:20 -0300

    Ejercicios 1 a 6
    Clase 1

--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -4,6 +4,9 @@
 */
 function Mostar()
 {
-	
+	var nombre;
+	nombre = prompt("ingrese su nombre","natalia natalia");
+
+	document.getElementById('elNombre').value = nombre;
 }
 
--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -4,5 +4,16 @@ Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
 
+var numero1;
+var numero2;
+var numero3;
+
+numero1 = parseInt (document.getElementById('numeroUno').value);
+numero2 = parseInt (document.getElementById('numeroDos').value);
+numero3 = numero1+numero2;
+
+alert(numero3);
+
+
 }
 
